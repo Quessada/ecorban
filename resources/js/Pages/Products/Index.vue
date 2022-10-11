4c8185f3a03391deb047bcfa90a12a9125d209d1
Quessada <matheusquessada@hotmail.com>
2022-10-11 19:58:12 -0300

    data table

--- resources/js/Pages/Products/Index.vue
+++ resources/js/Pages/Products/Index.vue
@@ -45,7 +45,7 @@ const form = useForm({
         "
       >
         <div>
-          <InputLabel for="name" value="Name" />
+          <InputLabel for="name" value="Nome" />
           <TextInput
             id="name"
             type="text"
@@ -58,7 +58,7 @@ const form = useForm({
         </div>
 
         <div class="mt-4">
-          <InputLabel for="code" value="Code" />
+          <InputLabel for="code" value="Código" />
           <TextInput
             id="code"
             type="text"
@@ -69,7 +69,7 @@ const form = useForm({
           <InputError class="mt-2" :message="form.errors.price" />
         </div>
         <div class="mt-4">
-          <InputLabel for="price" value="Price" />
+          <InputLabel for="price" value="Preço" />
           <TextInput
             id="price"
             type="text"
@@ -81,7 +81,7 @@ const form = useForm({
           <InputError class="mt-2" :message="form.errors.price" />
         </div>
         <div class="mt-4">
-          <InputLabel for="image" value="Image" />
+          <InputLabel for="image" value="Imagem" />
           <TextInput
             id="image"
             type="file"
@@ -94,15 +94,39 @@ const form = useForm({
         </div>
 
         <InputError :message="form.errors.message" class="mt-2" />
-        <PrimaryButton class="mt-4">Product</PrimaryButton>
+        <PrimaryButton class="mt-4">Criar</PrimaryButton>
       </form>
+    </div>
 
-      <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-        <Product
-          v-for="product in products"
-          :key="product.id"
-          :product="product"
-        />
+    <div class="w-full mt-10 shadow-sm rounded-lg divide-y">
+      <div class="max-w-3xl mx-auto">
+        <div class="relative overflow-x-auto sm:rounded-lg">
+          <table class="w-full text-left text-gray-500 dark:text-gray-400">
+            <thead
+              class="
+                text-xs text-gray-700
+                uppercase
+                bg-gray-50
+                dark:bg-gray-700 dark:text-gray-400
+              "
+            >
+              <tr>
+                <th scope="col" class="px-6 py-3">Nome Produto</th>
+                <th scope="col" class="px-6 py-3">Código</th>
+                <th scope="col" class="px-6 py-3">Preço</th>
+                <th scope="col" class="px-6 py-3">Imagem</th>
+                <th scope="col" class="px-6 py-3">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Product
+                v-for="product in products"
+                :key="product.id"
+                :product="product"
+              />
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   </AuthenticatedLayout>
